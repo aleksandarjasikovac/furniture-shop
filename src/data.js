65b23ee699893ec69c7d9d78ea89cfb9e32bab8f
alex-aestic <aleksandar.jasko@gmail.com>
2021-11-27 13:36:48 +0100

    Add components,  mock data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const shopItems = [
+    {
+    id:0,
+    slug:'songesand',
+    name:'Songesand',
+    description:'Lorem ipsum dolro sit amet. Ne znam sta jos ide, nije ni bitno'
+    },
+    {
+     id:1,
+     slug:'hovag',
+     name:'Hovag',
+     description:'Lorem ipsum dolro sit amet. Ne znam sta jos ide, nije ni bitno'
+     },
+
+{
+     id:2,
+      slug:'hovag',
+     name:'Hovag',
+      description:'Lorem ipsum dolro sit amet. Ne znam sta jos ide, nije ni bitno'
+     },
+
+     {
+        id:3,
+         slug:'enhet',
+        name:'Enhet',
+         description:'Lorem ipsum dolro sit amet. Ne znam sta jos ide, nije ni bitno'
+        },
+       
+    
+
+]
---
+++ src/pages/Storefront.js
@@ -0,0 +1,22 @@
+import { ShopItem } from "../components/ShopItem";
+import { shopItems } from "../data";
+import "./Storefront.css";
+
+export function Storefront() {
+    return (
+        <div className="container">
+            <div className="shop-items-list">
+
+                {shopItems.map((shopItem) => {
+                    return (
+                        <ShopItem
+                            key={shopItem.id}
+                            slug={shopItem.slug}
+                            name={shopItem.name}
+                            description={shopItem.description} />
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
